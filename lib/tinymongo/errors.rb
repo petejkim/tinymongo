d128522356dedee36f490f042a615cc67941b166
Peter Jihoon Kim <raingrove@gmail.com>
2010-07-22 11:35:52 +0900

    Version bump to 0.1.3

--- lib/tinymongo/errors.rb
+++ lib/tinymongo/errors.rb
@@ -4,19 +4,19 @@ module TinyMongo
   
   class NotConfiguredError < Error
     def initialize
-      super('Please do TinyMongo.configure() before attempting to connect.')
+      super('Please do TinyMongo.configure() before attempting to connect!')
     end
   end
   
   class NotConnectedError < Error
     def initialize
-      super('Not connected to MongoDB. Please connect using TinyMongo.connect().')
+      super('Not connected to MongoDB. Please connect using TinyMongo.connect()!')
     end
   end
   
   class ModifierOperationError < Error
     def initialize
-      super('Modifier operations are not allowed on objects that are not yet saved.')
+      super('Modifier operations are not allowed on objects that are not yet saved!')
     end
   end
 end
